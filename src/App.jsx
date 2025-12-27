fa226ba61bbb915723b06d3f828cff7899f912c4
JulioOsorio1 <1.julio.osorio@gmail.com>
2025-12-27 12:58:56 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,32 +20,32 @@ function App() {
       <NavBar />
 
       <Routes>
-        {/* Redirección */}
+        
         <Route path="/" element={<Navigate to="/home" replace />} />
 
-        {/* Home */}
+        
         <Route path="/home" element={<Home />} />
 
-        {/* Catálogo completo */}
+      
         <Route path="/animales" element={<ItemListContainer />} />
 
-        {/* Categorías */}
+       
         <Route
           path="/animales/categoria/:categoriaId"
           element={<ItemListContainer />}
         />
 
-        {/* Detalle */}
+       
         <Route path="/animales/detalle/:id" element={<ItemDetailContainer />} />
 
-        {/* Secciones */}
+        
         <Route path="/nosotros" element={<Nosotros />} />
         <Route path="/contacto" element={<Contacto />} />
 
-        {/* Carrito */}
+       
         <Route path="/cart" element={<Cart />} />
 
-        {/* 404 */}
+       
         <Route path="*" element={<NotFound />} />
       </Routes>
 
